2f51e2cee133993bd05583b99ed3c01f9c532e1d
KIMJONGJINN <enjoy1358@gmail.com>
2022-07-07 09:00:57 +0900

    Dijkstra

--- 0706/src/Q6_0706.java
+++ 0706/src/Q6_0706.java
@@ -1,157 +1,59 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Q6_0706 {
-    /* 신규 도로 */
-    /*
-     * 문제 이해
-     * 어떤 도시 A <-> 어떤 도시 B를 연결하는 도로 -> 그 비용이 1
-     * - 1-> N까지의 거리가 더 짧아질 수 있는 도로는 총 몇개 놓을 수 있는가?
-     * 
-     * - 1번 N번에는 도로를 놓을 수 없다.
-     * 
-     * # 간단하게 모든 지점의 쌍에 도로를 하나씩 놓아보고 -> dijkstra를 돌리면 되겠다.
-     * 
-     * 
-     */
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int T, N, M;
-    static ArrayList<Edge> al[];
-    static int[][] dist;
-    static int[] todist;
-    static int cnt;
 
-    static class Edge implements Comparable<Edge> {
-        int number;
-        int cost;
+    static class Edge implements Comparable <Edge> {
+        int num;
+        int cost; 
 
-        Edge(int number, int cost) {
-            this.number = number;
-            this.cost = cost;
+        Edge(int num, int cost) {
+            this.num = num;
+            this.cost = cost; 
         }
 
         @Override
         public int compareTo(Edge next) {
-            // TODO Auto-generated method stub
-            if (cost < next.cost)
+            if(cost < next.cost)
                 return -1;
-            if (cost > next.cost)
+            if(cost > next.cost)
                 return 1;
-
-            return 0;
+            return 0; 
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    static int test;
+    static int n, m;
+    static ArrayList<Edge>al[]; 
+    static int[] dist1;
+    static int[] distN; 
 
-        st = new StringTokenizer(br.readLine());
+    static void dijkstra(int node, int[] dist) {
+        // 1. pq 설정
+        PriorityQueue<Edge>pq = new PriorityQueue<>();
+        pq.add(new Edge(node, 0));
 
-        T = Integer.parseInt(st.nextToken());
+        // 2. dist설정
+        for(int i = 0; i <= n; i++)
+            dist[i] = Integer.MAX_VALUE;
+        dist[node] = 0; 
 
-        int start, end, c;
-
-        for (int j = 0; j < T; j++) {
-
-            st = new StringTokenizer(br.readLine());
-
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-
-            al = new ArrayList[N + 1];
-            dist = new int[3][N + 1];
-            todist = new int[N+1];
-            cnt =0;
-
-            for (int i = 1; i < N + 1; i++) {
-                al[i] = new ArrayList<>();
-                dist[0][i] = Integer.MAX_VALUE;
-                dist[1][i] = Integer.MAX_VALUE;
-                dist[2][i] = Integer.MAX_VALUE;
+        // 3. dijkstra
+        while(!pq.isEmpty()) {
+            Edge now = pq.poll();
+            for(int i = 0; i < al[now.num].size(); i++) {
+                Edge next = al[now.num].get(i);
+                // 거리 갱신
+                int ncost = dist[now.num] + next.cost;
+                if(dist[next.num] <= ncost)
+                    continue;
+                dist[next.num] = ncost;
+                pq.add(new Edge(next.num, ncost));
             }
-
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                start = Integer.parseInt(st.nextToken());
-                end = Integer.parseInt(st.nextToken());
-                c = Integer.parseInt(st.nextToken());
-
-                al[start].add(new Edge(end, c));
-                al[end].add(new Edge(start, c));
-            }
-
-
-
-            Dijkstra(1,0);
-
-            Dijkstra(N,1);
-
-            /*
-            for (int i = 1; i < N + 1; i++)
-                System.out.println("1 에서 " + i + "까지 시간" + " : " + dist[0][i]);
-        
-            for (int i = 1; i < N + 1; i++)
-                System.out.println("10 에서 " + i + "까지 시간" + " : " + dist[1][i]);
-            */
-
-
-
-             // 이렇게 구하면 N이 최대값인 경우 터진다.
-             /*
-            for(int i=2;i<N;i++){
-                for(int k=2;k<N;k++){
-                    if(i == k)
-                        continue;
-
-                    if(dist[0][N] > dist[0][i] + dist[1][k] + 1)
-                    {
-                        al[i].add(new Edge(k, 1));
-                        al[k].add(new Edge(i, 1));
-
-                        Dijkstra(1,2);
-
-                        if(dist[2][N] < dist[0][N])
-                            cnt ++;
-                        
-                        Arrays.fill(dist[2], Integer.MAX_VALUE);
-                    }
-
-
-
-                }
-            }
-            */
-            dist[1][0] = Integer.MAX_VALUE;
-            dist[1][1] = Integer.MAX_VALUE;
-            dist[1][N] = Integer.MAX_VALUE;
-            Arrays.sort(dist[1]);
-            //PS를 해야한다.
-            
-            int ans = 0;
-            // 모든 1 -> A => 모든 A도시를 기준으로 value가 맞는 값들을 찾아볼것
-            // 2번 도시부터 (1번에는 도로 못놓음), n번에도 못놓음 
-            for(int i = 2; i < N; i++) {
-                // distN[B] < dist1[N] - dist1[A] - 1; 
-                // distN[B] < value 
-                int value = dist[1][N] - dist[1][i] - 1;
-                // distN[B] < dist1[N] - dist1[A] - 1 을 충족하는 B도시의 개수를 빠르게 찾아서 
-                // ans에 누적 
-                ans += psearch(value, dist[1]);
-            }
-
-            System.out.println("#"+ (j+1) + " " + ans);
         }
-
     }
 
     // dist = dist[N] 
@@ -159,7 +61,7 @@ public class Q6_0706 {
         // left right를 설정
         // distN 기준 0~n-3
         int left = 0;
-        int right = N - 3; 
+        int right = n - 3; 
 
         int ans = 0; 
         // psearch 
@@ -181,28 +83,76 @@ public class Q6_0706 {
     }
 
 
-    static void Dijkstra(int node, int k) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(node, 0));
-        dist[k][node] = 0;
-
-        while (!pq.isEmpty()) {
-            Edge now = pq.poll();
-            for (int i = 0; i < al[now.number].size(); i++) {
-                Edge next = al[now.number].get(i);
-
-                int nextCost = dist[k][now.number] + next.cost;
-
-                if (nextCost >= dist[k][next.number])
-                    continue;
-
-                dist[k][next.number] = nextCost;
-                pq.add(new Edge(next.number, nextCost));
+    public static void main(String[] args) throws IOException {
+        test = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= test; t++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            // arraylist init
+            al = new ArrayList[n+1];
+            for(int i = 0; i <= n; i++)
+                al[i] = new ArrayList<>();
+            // edge input
+            for(int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                int cost = Integer.parseInt(st.nextToken());
+                // 양방향 연결
+                al[from].add(new Edge(to, cost));
+                al[to].add(new Edge(from , cost));
             }
 
+            // 힌트 1
+            // dijkstra
+            // 1->모든 노드의 최단거리 dist1
+            dist1 = new int[n+1];
+            // N->몯느 노드의 최단거리 distN
+            distN = new int[n+1];
+
+            // dijkstra 2번 --> 시간복잡도=2 x O(ElogE)
+            dijkstra(1, dist1);
+            dijkstra(n, distN); 
+
+            // 힌트 2 
+            // 1->n, n->1 모두 알고잇는데도 불구하고, 모든 도로를 놓아볼수 X (100,000^2) 
+            // 가능성이 있는 노드만 연결 
+            // 1 -> A -> B -> N + 1 < dist1[N]; 
+            // 1 -> A라는 노드까지의 거리를 A
+            // N -> B라는 노드까지의 거리를 B
+            // A + B + 1 < dist1[N];
+            // B < dist1[N] - A - 1; 
+            // distN[B] < dist1[N] - dist1[A] - 1; 
+            // dist1[N] - dist1[A] - 1 = value
+            // distN[B] < value 
+            // ^ parametric search로 빠르게 찾을 것
+
+
+            // 처리를 몇개 해줘야 함
+            // 1. 1에 둘수 없죠
+            // 2. N에 둘수 없죠
+            // 3. 0은 저희가 사용하지 않습니다
+            // --> 얘네들을 걸러주기 위해서 --> 맨 오른쪽으로 push 
+            distN[0] = Integer.MAX_VALUE;
+            distN[1] = Integer.MAX_VALUE;
+            distN[n] = Integer.MAX_VALUE;
+
+            // distN 정렬
+            Arrays.sort(distN);
+
+            int ans = 0;
+            // 모든 1 -> A => 모든 A도시를 기준으로 value가 맞는 값들을 찾아볼것
+            // 2번 도시부터 (1번에는 도로 못놓음), n번에도 못놓음 
+            for(int i = 2; i < n; i++) {
+                // distN[B] < dist1[N] - dist1[A] - 1; 
+                // distN[B] < value 
+                int value = dist1[n] - dist1[i] - 1;
+                // distN[B] < dist1[N] - dist1[A] - 1 을 충족하는 B도시의 개수를 빠르게 찾아서 
+                // ans에 누적 
+                ans += psearch(value, distN);
+            }
+            System.out.println("#" + t + " " + ans);    
         }
-
     }
-
-
 }
